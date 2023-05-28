fc96b189468dfdb23f71d6031eceefd286d99125
Adharsh Reghu <adharshreghu2002@gmail.com>
2023-05-29 02:08:45 +0530

    final commit

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 const container = document.querySelector('.container');
 const btn = container.querySelector('button');
-
-btn.addEventListener('click',(e)=>{
+const input = container.querySelector('form input');
+const image = container.querySelector('.Qr img');
+const qrsize='200x200'
+const url = 'http://api.qrserver.com/v1/create-qr-code/';
+async function getImage (e){
     e.preventDefault();
-    container.classList.add('active')});
+    const inputValue = input.value;
+    if(!inputValue){
+        return
+    }
+    btn.innerText='Generating QR Code....';
+    const response =  await fetch(`${url}?data=${inputValue}&size=${qrsize}`);
+    const source = response.url
+    image.src= source;
+    
+    image.addEventListener('load',()=>{
+        btn.innerText='Generate QR Code';
+        container.classList.add('active')
+    })
+}
+btn.addEventListener('click',getImage);
+
+input.addEventListener('keyup',()=>{
+    if(!input.value){
+        container.classList.remove('active');
+    }
+})
